c467be379747332d25df398216f3a5a6d8b53e33
MagneticMartian <60756647+MagneticMartian@users.noreply.github.com>
2023-10-02 18:12:16 -0500

    Add files via upload

--- standard_patterns.zig
+++ standard_patterns.zig
@@ -1,5 +1,8 @@
 const std = @import("std");
 const expect = std.testing.expect;
+const eql = std.mem.eql;
+const ArrayList = std.ArrayList;
+const test_allocator = std.testing.allocator;
 
 // This is the simplest allocator. It request pages of
 // memory based on the amount you give.
@@ -59,11 +62,7 @@ test "GPA" {
     defer allocator.free(bytes);
 }
 
-// Array list is a resizable array similar to std::vector<T> in c++
-const eql = std.mem.eql;
-const ArrayList = std.ArrayList;
-const test_allocator = std.testing.allocator;
-
+// Arraylist is a resizable array similar to std::vector<T> in c++
 test "ArrayList" {
     var list = ArrayList(u8).init(test_allocator);
     defer list.deinit();
@@ -126,4 +125,96 @@ test "make_dir" {
         if (entry.kind == .file) file_count += 1;
     }
     try expect(file_count == 3);
+}
+test "io writer usage" {
+    var list = ArrayList(u8).init(test_allocator);
+    defer list.deinit();
+    const bytes_written = try list.writer().write(
+        "Hello World!",
+    );
+    try expect(bytes_written == 12);
+    try expect(eql(u8, list.items, "Hello World!"));
+}
+test "io reader usage" {
+    const message = "Hello File!";
+    const file = try std.fs.cwd().createFile(
+        "junk_file2.txt",
+        .{ .read = true },
+    );
+    defer file.close();
+
+    try file.writeAll(message);
+    try file.seekTo(0);
+
+    const contents = try file.reader().readAllAlloc(
+        test_allocator,
+        message.len, // if file larger than this: error.StreamTooLong
+    );
+    defer test_allocator.free(contents);
+
+    try expect(eql(u8, contents, message));
+}
+
+fn nextLine(reader: anytype, buffer: []u8) !?[]const u8 {
+    var line = try (reader.readUntilDelimiterOrEof(
+        buffer,
+        '\n',
+    )) orelse return null;
+    // DOWN WITH WINDOWS
+    if (@import("builtin").os.tag == .windows) {
+        return std.mem.trimRight(u8, line, "\r");
+    } else {
+        return line;
+    }
+}
+test "read until next line" {
+    const stdout = std.io.getStdOut();
+    const stdin = std.io.getStdIn();
+
+    // this is similar to std::cout
+    try stdout.writeAll(
+    // this is not a comment
+        \\ Enter Your name: 
+    );
+
+    var buffer: [100]u8 = undefined;
+    const input = (try nextLine(stdin.reader(), &buffer)).?;
+    try stdout.writer().print(
+        "Your name is: \"{s}\"\n",
+        .{input},
+    );
+}
+const MyByteList = struct {
+    data: [100]u8 = undefined, // This is for demo only. ArrayList is better
+    items: []u8 = &[_]u8{},
+
+    const Writer = std.io.Writer(
+        *MyByteList,
+        error{EndOfBuffer},
+        appendWrite,
+    );
+    fn appendWrite(
+        self: *MyByteList,
+        data: []const u8,
+    ) error{EndOfBuffer}!usize {
+        if (self.items.len + data.len > self.data.len) {
+            return error.EndOfBuffer;
+        }
+        std.mem.copy(
+            u8,
+            self.data[self.items.len..],
+            data,
+        );
+        self.items = self.data[0 .. self.items.len + data.len];
+        return data.len;
+    }
+    fn writer(self: *MyByteList) Writer {
+        return .{ .context = self };
+    }
+};
+test "custom writer" {
+    var bytes = MyByteList{};
+    _ = try bytes.writer().write("Hello");
+    _ = try bytes.writer().write(" Writer!");
+    try expect(eql(u8, bytes.items, "Hello Writer!"));
 }
